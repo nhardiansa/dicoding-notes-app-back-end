01aaeea897f232fe661188a09bfa7c3e51adfebc
nhardiansa <nhardiansa@gmail.com>
2021-04-14 14:37:54 +0800

    memperbaiki bug

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '54.169.216.115',
     routes: {
       cors: {
         origin: ['*'],
